42e65c1451fcdf07c55c4366f7595904eacb4c47
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-13 21:31:57 +0300

    задача один

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}.padStart(6, '0')}`;
+}
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClickColor);
+refs.stopBtn.addEventListener('click', onStartBtnClickColor);
+
+
+   function onStartBtnClickColor() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+} 
+
+function onStartBtnClickColor() {
+  clearInterval(timerId);
+
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+
+
