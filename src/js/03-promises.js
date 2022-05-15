d3d330c21b73052e8311e487f0c20aabe8f58431
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-15 17:01:09 +0300

    3 задача

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const delay = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
+
+
+  formPromises(delay, step, amount);
+
 }
+
+
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+
+for (let i = 1; i <= amount; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      });
+    delay += step;
+  }
